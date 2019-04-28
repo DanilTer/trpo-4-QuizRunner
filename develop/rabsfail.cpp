07582d8e9a4b1ab38f130aeaddc4962a3e518483
Andrey <a.ya.v.snuper@mail.ru>
2019-04-28 17:54:22 +0700

    develop

---
+++ develop/rabsfail.cpp
@@ -0,0 +1,54 @@
+#include <conio.h>
+#include <stdlib.h>
+#include <clocale>
+#include <ctime>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+main()
+{	
+int vopros, player, stroka, otvet, totvet, prodolzit;   
+	setlocale(LC_ALL, "rus"); // корректное отображение Кириллицы
+    char slovo[1000]; // буфер промежуточного хранения считываемого из файла текста
+    ifstream fin("quizrunner.txt"); // открыли файл для чтения
+ for(vopros = 0; vopros<10; vopros++)
+ {
+    for(stroka = 0; stroka<6; stroka++)
+	{
+		fin.getline(slovo, 1000); // считали строку из файла
+    	if(stroka==5)
+    		{
+    			totvet=slovo[0];
+				scanf("%d",&player);
+				if(totvet == player)
+				{
+					otvet++;
+					continue;	
+				}	
+				else
+				{
+					printf ("\nПравильные ответ: %d",totvet);
+						    printf ("\nНажмите на любую кнопку: ");
+						    scanf("%d",&prodolzit);
+						    if (prodolzit == 0)
+						    {
+						    	;
+							}
+				}
+			}
+    	cout << slovo << endl; // напечатали эту строку
+ 	}
+ }
+    fin.close();
+    setlocale(LC_ALL, "rus");
+	ofstream fout;
+	fout.open("result.txt");
+	fout << "ТЕсты";
+	fout.close();
+    
+	system("pause");
+    return 0;
+}
