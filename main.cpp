56df5165b59f0300c1f023d7718ca335347ddad2
Andrey <a.ya.v.snuper@mail.ru>
2019-04-28 19:28:11 +0700

    ispravlena oshipka s getchar()

--- main.cpp
+++ main.cpp
@@ -3,53 +3,51 @@
 #include <clocale>
 #include <ctime>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 main()
 {
-int player, otvet=0, totvet, prodolzit;
-	printf("1. Vopros");	
-		totvet = 3;
-		printf("\n1. Otvet");
-		printf("\n2. Otvet");
-		printf("\n3. Otvet");
-		printf("\n4. Otvet\n");
-		    printf("Otvet: ");
+int player, otvet=0, totvet[2]={2,4}, prodolzit, ch, i=0, vopros, stroka;	
+setlocale(LC_ALL, "rus"); // корректное отображение Кириллицы
+    char slovo[1000]; // буфер промежуточного хранения считываемого из файла текста
+    ifstream fin("quizrunner.txt"); // открыли файл для чтения
+ for(vopros = 0; vopros<2; vopros++,i++)
+ {
+    for(stroka = 0; stroka<6; stroka++)
+	{
+		fin.getline(slovo, 1000); // считали строку из файла
+    	if(stroka==5)
+    		{
+    			//totvet[i]=slovo[0];
 				scanf("%d",&player);
-		    		if(player == totvet)
-		    			{
-		    			otvet++;	
-						}
-						else
-						{
-							printf ("\nPravilniy otvet: %d",totvet);
-						    printf ("\nVvedite luboe chislo dlya prodolzeniya: ");
-						    scanf("%d",prodolzit);
-						}
-	system("CLS");	
-	printf("2. Vopros");
-		totvet = 1;
-		printf("\n1. Otvet");
-		printf("\n2. Otvet");
-		printf("\n3. Otvet");
-		printf("\n4. Otvet\n");
-			printf("Otvet: ");
-				scanf("%d",&player);
-		        		if(player == 1)
-		    			{
-		    			otvet++;	
-						}
-						else
-						{
-							printf ("\nPravilniy otvet: %d",totvet);
-							printf ("\nVvedite luboe chislo dlya prodolzeniya: ");
-						    getch();
-						}
-	system("CLS");
-	
-	printf("Vas resultat:\n");
-	printf("Kol-vo pravylnyh otvetov %d/2",otvet);
+				if(totvet[i] == player)
+				{
+					otvet++;
+					system("CLS");
+					continue;	
+				}	
+				else
+				{
+					printf ("\nПравильные ответ: %d",totvet[i]);
+					printf ("\nНажмите на любую кнопку: \n");
+				    if (ch != 13 or ch != 32)
+				    {
+						cin.get();
+						ch = getch();
+				    	system("CLS");
+				    	continue;
+					}
+				}
+			}
+    	cout << slovo << endl; // напечатали эту строку
+ 	}
+ }			
+printf("Ваш результат:\n");
+printf("Кол-во правильных ответов: %d/2",otvet);
+system("PAUSE");
+return 0;
 }
   
 
